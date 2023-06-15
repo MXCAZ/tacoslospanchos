05cd72f27ec14ef150c865fe6706f8853011409a
MXCAZ <final00717@hotmail.com>
2023-06-15 16:32:29 -0600

    .

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,20 +1,10 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 
+import BestPlates from "../pages/Menu/BestPlates";
 import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu";
 
-// import OrderOnline from "../pages/Order/Order";
-
 import { AnimatePresence } from "framer-motion";
-// import OrderScreen from "../pages/Order/screens/OrderScreen";
-// import ChoseScreen from "../pages/Order/screens/ChoseScreen";
-// import ReviewScreen from "../pages/Order/screens/ReviewScreen";
-// import SelectPaymentScreen from "../pages/Order/screens/selectPaymentScreen";
-// import PaymentScreen from "../pages/Order/screens/PaymentScreen";
-// import CompleteOrderScreen from "../pages/Order/screens/CompleteOrderScreen";
-// import AdminScreen from "../pages/Order/screens/AdminScreen";
-// import QueueScreen from "../pages/Order/screens/QueueScreen";
-import BestPlates from "../pages/Menu/BestPlates";
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -25,8 +15,7 @@ const AnimatedRoutes = () => {
           <Route path="/" element={<Home />} />
           <Route path="/Menu" element={<Menu />} />
           <Route path="/BestPlates" element={<BestPlates />} />
-          <Route path="/Menu" element={<Menu />} />
-          <Route path="/BestPlates" element={<BestPlates />} />
+
           <Route path="*" element={<Home />} />
         </Routes>
       </AnimatePresence>
@@ -35,8 +24,16 @@ const AnimatedRoutes = () => {
 };
 export default AnimatedRoutes;
 
-{
-  /* <Route path="/OrderOnline" element={<OrderOnline />} />
+// import OrderOnline from "../pages/Order/Order";
+// import OrderScreen from "../pages/Order/screens/OrderScreen";
+// import ChoseScreen from "../pages/Order/screens/ChoseScreen";
+// import ReviewScreen from "../pages/Order/screens/ReviewScreen";
+// import SelectPaymentScreen from "../pages/Order/screens/selectPaymentScreen";
+// import PaymentScreen from "../pages/Order/screens/PaymentScreen";
+// import CompleteOrderScreen from "../pages/Order/screens/CompleteOrderScreen";
+// import AdminScreen from "../pages/Order/screens/AdminScreen";
+// import QueueScreen from "../pages/Order/screens/QueueScreen";
+/* <Route path="/OrderOnline" element={<OrderOnline />} />
           <Route path="/Choose" element={<ChoseScreen />} />
           <Route path="/Order" element={<OrderScreen />} />
           <Route path="/Review" element={<ReviewScreen />} />
@@ -45,4 +42,3 @@ export default AnimatedRoutes;
           <Route path="/complete" element={<CompleteOrderScreen />} />
           <Route path="/admin" element={<AdminScreen />} />
           <Route path="/queue" element={<QueueScreen />} /> */
-}
