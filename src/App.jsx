ca5d199ce31e7753aa824c29af4a317dc5066127
MXCAZ <final00717@hotmail.com>
2023-06-15 15:43:41 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import PageRoutes from "./components/routes";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { StoreProvider } from "./pages/Order/Store";
+// import { StoreProvider } from "./pages/Order/Store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StoreProvider>
+  <React.StrictMode>
     <App />
-  </StoreProvider>
+  </React.StrictMode>
 );
