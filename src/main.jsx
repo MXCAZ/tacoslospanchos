f07abcae6e1a3d86493e3187ff8ee4d79deb3f3f
MXCAZ <final00717@hotmail.com>
2023-06-15 15:54:52 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// import { StoreProvider } from "./pages/Order/Store";
+import { StoreProvider } from "./pages/Order/Store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
